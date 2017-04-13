0a463be88ed7986fff829beef4f3aee1f8c9aa46
Zachary Viggiani <zach@bigmojo.net>
2017-04-13 14:02:44 -0400

    Part one done

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413172908) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
-  end
+  end 
 
 end
